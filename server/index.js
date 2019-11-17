33cc6ddb2b81a510ae30d3d3c522a0f77d57cd6b
wangguilin <2351268139@qq.com>
2019-11-17 22:23:43 +0800

    xiugai

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ app.use('/', express.static(__dirname + '/web'))
 app.use('/admin', express.static(__dirname + '/admin'))
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
-require('./plugins/db')(app)
-require('./routes/admin')(app)
-require('./routes/web')(app)
+// require('./plugins/db')(app)
+// require('./routes/admin')(app)
+// require('./routes/web')(app)
 
 app.listen(3000, () => {
   console.log('http://localhost:3000');
